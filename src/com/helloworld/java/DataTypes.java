d50131a74cc469a543fb9395a3e0e11f4a7740cc
Admin <chellurivenkataajay@gmail.com>
2024-10-29 14:50:23 +0530

    chore:add model classes like datatypes and helloworld

---
+++ src/com/helloworld/java/DataTypes.java
@@ -0,0 +1,70 @@
+package com.helloworld.java;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+        //single line comment
+
+        /*multi line comments*/
+
+        // data types
+        /*
+        1. primitive
+        2. non primitive
+
+          1. primitive
+        1.byte(1byte)
+        2.short  2
+        3.int  4
+        4.long  8
+        5.float 4
+        6.double 8
+        7.char 1
+        8 boolean 2
+
+        1 byte 4  -128 to 127
+         2. non primitive
+
+         objects
+         arrays
+         string
+         */
+
+        byte age;
+        age = 21;
+        System.out.println("enter the age value:" + age);
+
+        short id;
+        id = 12343;
+        System.out.println("enter the id value: " + id);
+
+        int num;
+        num = 93981212;
+        System.out.println("enter the num:" + num);
+
+        long mob;
+        mob = 9666462662L;
+        System.out.println("the mobile num:" + mob);
+
+        float dec = 2456789098765456787654898765456798769f;
+        System.out.println("print the value:" + dec);
+
+        double ref;
+        ref = 32.4D;
+        System.out.println("print the double value of:" + ref);
+
+        char sin;
+        sin = 'c';
+        System.out.println("the symbol:" + sin);
+
+        boolean isboolean;
+        isboolean = true;
+        System.out.println("the boolean val:" + isboolean);
+
+        String vasu;
+        vasu = "Deepika";
+        System.out.println("enter the value of vasu:" + vasu);
+
+
+    }
+}
